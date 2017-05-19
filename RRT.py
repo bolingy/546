e0ff64f883ac75e184a09a68beffd71a38387fc5
bolingy <bolingy@uw.edu>
2017-05-18 22:20:35 -0700

    changed sth

--- RRT.py
+++ RRT.py
@@ -9,17 +9,72 @@ class Node(object):
         self.parent = parent
 
 class RRT():
-    def __init__(self, xd, yd, epsilon, nodeNum):
+    def __init__(self, xd, yd, epsilon, nodeNum, rectObs, tree):
         self.XDIM = xd
         self.YDIM = yd
-        self.windowSize = [XDIM, YDIM]
         self.EPSILON = epsilon
         self.NUMNODES = nodeNum
-        self.GOAL_RADIUS = 10
-        self.MIN_DISTANCE_TO_ADD = 1.0
-        self.white = 255, 255, 255
-        self.black = 0, 0, 0
-        self.red = 255, 0, 0
-        self.blue = 0, 255, 0
-        self.green = 0, 0, 255
-        self.cyan = 0, 180, 105
+        self.rectObs = rectObs
+        self.count = 0
+        self.nodes = tree
+
+    def dist(self, p1, p2):
+        # distance between two points
+        return sqrt((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]))
+
+    def point_circle_collision(self, p1, p2, radius):
+        # check goal area
+        distance = self.dist(p1, p2)
+        if (distance <= radius):
+            return True
+        return False
+
+    def step_from_to(self, p1, p2):
+        # generate point toward p2
+        if self.dist(p1, p2) < self.EPSILON:
+            return p2
+        else:
+            theta = atan2(p2[1] - p1[1], p2[0] - p1[0])
+            return p1[0] + self.EPSILON * cos(theta), p1[1] + self.EPSILON * sin(theta)
+
+    def collides(self, p):
+        # check if point collides with the obstacle
+        for rect in self.rectObs:
+            if rect.collidepoint(p) == True:
+                return True
+        return False
+
+    def get_random_clear(self):
+        # return a random point with collision check
+        while True:
+            p = random.random() * self.XDIM, random.random() * self.YDIM
+            noCollision = self.collides(p)
+            if noCollision == False:
+                return p
+
+    def explore(self, screen, color, goalPoint, goalNode, GOAL_RADIUS = 10):
+        # RRT
+        while True:
+            self.count = self.count + 1
+            pygame.display.set_caption('Performing RRT')
+            if self.count < self.NUMNODES:
+                foundNext = False
+                while foundNext == False:
+                    rand = self.get_random_clear()
+                    closestNode = self.nodes[0]
+                    for p in self.nodes:
+                        if self.dist(p.point, rand) <= self.dist(closestNode.point, rand):
+                            newPoint = self.step_from_to(p.point, rand)
+                            if self.collides(newPoint) == False:
+                                closestNode = p
+                                foundNext = True
+
+                newnodePoint = self.step_from_to(closestNode.point, rand)
+                self.nodes.append(Node(newnodePoint, closestNode))
+                pygame.draw.line(screen, color, closestNode.point, newnodePoint)
+                if self.point_circle_collision(newnodePoint, goalPoint.point, GOAL_RADIUS):
+                    goalNode = self.nodes[len(self.nodes) - 1]
+                    return 'goalFound'
+            else:
+                print("exploration done")
+                return;
--- SRT.py
+++ SRT.py
@@ -1,6 +1,8 @@
 import math, sys, pygame, random
 from math import *
 from pygame import *
+from scipy import spatial
+import RRT
 
 '''
 K: number of milestones(tress)
@@ -18,5 +20,123 @@ class SRT():
         self.nr = nr
         self.np = np
         self.ni = ni
+        self.Vt = [] # list of trees
+        self.Et = []
+        self.Q = []
+        self.Ec = []
+        #self.kdTree = spatial.KDTree(self.Q) # for nearest neighbor
+        # pygame
+        pygame.init()
+        self.XDIM = 720
+        self.YDIM = 500
+        self.windowSize = [self.XDIM, self.YDIM]
+        self.GOAL_RADIUS = 10
+        self.MIN_DISTANCE_TO_ADD = 1.0
+        self.white = 255, 255, 255
+        self.black = 0, 0, 0
+        self.red = 255, 0, 0
+        self.blue = 0, 255, 0
+        self.green = 0, 0, 255
+        self.cyan = 0, 180, 105
+        self.fpsClock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode(self.windowSize)
+        self.rectObs = []
 
-    
+    def init_obstacles(self, configNum):
+        # initialized the obstacle, configNum select pre-set obstacles
+        self.rectObs = []
+        print("config " + str(configNum))
+        if (configNum == 0):
+            self.rectObs.append(pygame.Rect((self.XDIM / 2.0 - 50, self.YDIM / 2.0 - 100), (100, 200)))
+        if (configNum == 1):
+            self.rectObs.append(pygame.Rect((100, 50), (200, 150)))
+            self.rectObs.append(pygame.Rect((400, 200), (200, 100)))
+            self.rectObs.append(pygame.Rect((20, 100), (330, 100)))
+        if (configNum == 2):
+            self.rectObs.append(pygame.Rect((100, 50), (200, 150)))
+        if (configNum == 3):
+            self.rectObs.append(pygame.Rect((100, 50), (200, 150)))
+
+        for rect in self.rectObs:
+            pygame.draw.rect(self.screen, self.black, rect)
+
+    def reset(self):
+        # set pygame and obstacles
+        self.screen.fill(self.white)
+        self.init_obstacles(1)
+
+    def collides(self, p):
+        # check if point collides with the obstacle
+        for rect in self.rectObs:
+            if rect.collidepoint(p) == True:
+                return True
+        return False
+
+    def rrt_demo(self):
+        initPoseSet = False
+        initialPoint = RRT.Node(None, None)
+        goalPoseSet = False
+        goalPoint = RRT.Node(None, None)
+        currentState = 'init'
+
+        nodes = []
+        self.reset()
+        goalNode = RRT.Node(None, None)
+
+        while True:
+            # pygame wait for inital and final point
+            if currentState == 'init':
+                pygame.display.set_caption('Select Starting Point and then Goal Point')
+                self.fpsClock.tick(10)
+            elif currentState == 'goalFound':
+                currNode = goalNode.parent
+                pygame.display.set_caption('Goal Reached')
+                while currNode.parent != None:
+                    pygame.draw.line(self.screen, self.red, currNode.point, currNode.parent.point)
+                    currNode = currNode.parent
+                optimizePhase = True
+            elif currentState == 'optimize':
+                self.fpsClock.tick(0.5)
+                pass
+            elif currentState == 'buildTree':
+                tree = RRT.RRT(self.XDIM, self.YDIM, 10, 500, self.rectObs, nodes)
+                currentState = tree.explore(self.screen, self.cyan, goalPoint, goalNode, GOAL_RADIUS = 10)
+
+            # handle events
+            for e in pygame.event.get():
+                if e.type == QUIT or (e.type == KEYUP and e.key == K_ESCAPE):
+                    sys.exit("Exiting")
+                if e.type == MOUSEBUTTONDOWN:
+                    print('mouse down')
+                    if currentState == 'init':
+                        if initPoseSet == False:
+                            nodes = []
+                            if self.collides(e.pos) == False:
+                                print('initiale point set: ' + str(e.pos))
+
+                                initialPoint = RRT.Node(e.pos, None)
+                                nodes.append(initialPoint)  # Start in the center
+                                initPoseSet = True
+                                pygame.draw.circle(self.screen, self.red, initialPoint.point, 10)
+                        elif goalPoseSet == False:
+                            print('goal point set: ' + str(e.pos))
+                            if self.collides(e.pos) == False:
+                                goalPoint = RRT.Node(e.pos, None)
+                                goalPoseSet = True
+                                pygame.draw.circle(self.screen, self.green, goalPoint.point, 10)
+                                currentState = 'buildTree'
+                    else:
+                        currentState = 'init'
+                        initPoseSet = False
+                        goalPoseSet = False
+                        self.reset()
+
+            pygame.display.update()
+            self.fpsClock.tick(10000)
+
+
+
+if __name__ == '__main__':
+    srt = SRT( 10, 10, 10, 10, 10, 10)
+    srt.init_obstacles(1)
+    srt.rrt_demo()
